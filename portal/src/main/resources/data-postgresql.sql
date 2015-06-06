53960f01fd6529dbd75b537f09cd1dc622852374
borceavirgil@gmail.com <virgil21>
2015-06-06 13:49:05 +0300

    * password

--- portal/src/main/resources/data-postgresql.sql
+++ portal/src/main/resources/data-postgresql.sql
@@ -1,4 +1,4 @@
 INSERT INTO USERS (user_id, password, email, first_name, last_name, login_ip_address, created_date, active)
-VALUES (3, 'admin', 'admin@portal_auto.ro', 'admin', 'admin', '192.168.80.102', now(), true);
+VALUES (3, '7c222fb2927d828af22f592134e8932480637c0d', 'admin@portal_auto.ro', 'admin', 'admin', '192.168.80.102', now(), true);
 INSERT INTO USERS (user_id, password, email, first_name, last_name, login_ip_address, created_date, active)
-VALUES (4, 'superuser', 'superuser@portal_auto.ro', 'Prosop', 'Titi', '192.168.80.25', now(), true);
+VALUES (4, '7c222fb2927d828af22f592134e8932480637c0d', 'superuser@portal_auto.ro', 'Prosop', 'Titi', '192.168.80.25', now(), true);
