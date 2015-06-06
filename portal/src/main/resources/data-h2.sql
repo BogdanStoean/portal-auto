ffe647984e37cc89c94db2766471d9c042679604
borceavirgil@gmail.com <virgil21>
2015-06-06 13:48:14 +0300

    * password

--- portal/src/main/resources/data-h2.sql
+++ portal/src/main/resources/data-h2.sql
@@ -1,6 +1,6 @@
 INSERT INTO USERS (user_id, password, email, first_name, last_name, login_ip_address, created_date, active)
-VALUES (10, 'titi', 'titi@portal_auto.ro', 'titi', 'titi', '192.168.80.101', now(), true);
+VALUES (10, '7c222fb2927d828af22f592134e8932480637c0d', 'titi@portal_auto.ro', 'titi', 'titi', '192.168.80.101', now(), true);
 INSERT INTO USERS (user_id, password, email, first_name, last_name, login_ip_address, created_date, active)
-VALUES (20, 'password', 'user@portal_auto.ro', 'user', 'masinuta', '192.168.80.102', now(), true);
+VALUES (20, '7c222fb2927d828af22f592134e8932480637c0d', 'user@portal_auto.ro', 'user', 'masinuta', '192.168.80.102', now(), true);
 INSERT INTO USERS (user_id, password, email, first_name, last_name, login_ip_address, created_date, active)
-VALUES (30, 'admin1', 'admin1@portal_auto.ro', 'admin1', 'admin1', '192.168.80.102', now(), true);
+VALUES (30, '7c222fb2927d828af22f592134e8932480637c0d', 'admin1@portal_auto.ro', 'admin1', 'admin1', '192.168.80.102', now(), true);
