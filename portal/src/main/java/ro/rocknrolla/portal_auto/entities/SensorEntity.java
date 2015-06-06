93320a72182d3feacf34a1a461558b8a4ab96d27
borceavirgil@gmail.com <virgil21>
2015-06-06 13:22:58 +0300

    * adding sensor entity

---
+++ portal/src/main/java/ro/rocknrolla/portal_auto/entities/SensorEntity.java
@@ -0,0 +1,40 @@
+package ro.rocknrolla.portal_auto.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class SensorEntity extends AbstractEntity<SensorEntity>{
+
+    @Id
+    @Column(name = "SENSOR_ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SENSOR_SEQ_GEN")
+    @SequenceGenerator(name = "SENSOR_SEQ_GEN", sequenceName = "SENSOR_ID_SEQ", allocationSize = 1)
+    private Long id;
+
+    private String name;
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
