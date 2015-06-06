bac75393b386c67544652bbfb2bfe136fe6d3ef0
Bogdan Stoean <stoeanbogdan89@gmail.com>
2015-06-06 17:45:07 +0300

    restful services for car sensor critical values

--- portal/src/main/java/ro/rocknrolla/portal_auto/service/UserCarService.java
+++ portal/src/main/java/ro/rocknrolla/portal_auto/service/UserCarService.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ro.rocknrolla.portal_auto.controller.bean.CarModel;
 import ro.rocknrolla.portal_auto.entities.Car;
+import ro.rocknrolla.portal_auto.exception.ServerEntityNotFoundException;
 import ro.rocknrolla.portal_auto.repositories.CarRepository;
+import ro.rocknrolla.portal_auto.repositories.UserRepository;
+import ro.rocknrolla.portal_auto.security.CurrentAuthenticatedUser;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
@@ -21,12 +24,37 @@ public class UserCarService {
     @Autowired
     private CarRepository carRepository;
 
-    public List<CarModel> getUserCars(String userEmail){
-        List<Car> cars =  carRepository.findByUserEmail(userEmail);
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<CarModel> getUserCars(String userEmail) {
+        List<Car> cars = carRepository.findByUserEmail(userEmail);
         List<CarModel> results = new ArrayList<>();
         for (Car car : cars) {
             results.add(new CarModel(car));
         }
         return results;
+    }
+
+    public void create(CarModel carModel) {
+
+        Car car;
+
+        if (carModel.getCarId() != null) {
+            car = carRepository.findOne(carModel.getCarId());
+        } else {
+            car = new Car();
+        }
+
+        if (car == null) {
+            throw new ServerEntityNotFoundException("id", "entity.not.found");
+        }
+
+        car.setActive(true);
+        car.setDeviceUUID(carModel.getDeviceUUID());
+        car.setName(carModel.getName());
+        car.setUser(userRepository.findActiveByEmail(CurrentAuthenticatedUser.getUsername()));
+
+        carRepository.save(car);
     }
 }
