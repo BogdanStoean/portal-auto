fa28d92e46aed9c5a9a36a16a02c0068b4acd50b
borceavirgil@gmail.com <virgil21>
2015-06-06 17:49:31 +0300

    * adding car parameter display DTO

---
+++ portal-dtos/src/main/java/ro/rocknrolla/common/SensorDisplayDTO.java
@@ -0,0 +1,33 @@
+package ro.rocknrolla.common;
+
+
+public class SensorDisplayDTO {
+
+    private String name;
+    private String status;
+    private String mesaj;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMesaj() {
+        return mesaj;
+    }
+
+    public void setMesaj(String mesaj) {
+        this.mesaj = mesaj;
+    }
+}
