47e236a5cc2fdbf803054733a115049efc7e9682
borceavirgil@gmail.com <virgil21>
2015-06-06 11:29:02 +0300

    * cleanup

--- portal/src/main/resources/data-h2.sql
+++ portal/src/main/resources/data-h2.sql
@@ -1,6 +1,6 @@
 INSERT INTO USERS (user_id, password, email, first_name, last_name, login_ip_address, created_date, active)
-VALUES (10, 'titi', 'titi@nemetschek.ro', 'titi', 'titi', '192.168.80.101', now(), true);
+VALUES (10, 'titi', 'titi@portal_auto.ro', 'titi', 'titi', '192.168.80.101', now(), true);
 INSERT INTO USERS (user_id, password, email, first_name, last_name, login_ip_address, created_date, active)
-VALUES (20, 'password', 'user@nemetschek.ro', 'user', 'masinuta', '192.168.80.102', now(), true);
+VALUES (20, 'password', 'user@portal_auto.ro', 'user', 'masinuta', '192.168.80.102', now(), true);
 INSERT INTO USERS (user_id, password, email, first_name, last_name, login_ip_address, created_date, active)
-VALUES (30, 'admin1', 'admin1@nemetschek.ro', 'admin1', 'admin1', '192.168.80.102', now(), true);
+VALUES (30, 'admin1', 'admin1@portal_auto.ro', 'admin1', 'admin1', '192.168.80.102', now(), true);
