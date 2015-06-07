34b516e7a7eb68e59e9b3d078b0634716140a556
Lucian Neghina <lucian@neghina.ro>
2015-06-07 07:27:57 +0300

    dashboard

--- portal/src/main/resources/static/js/controllers/DashboardController.js
+++ portal/src/main/resources/static/js/controllers/DashboardController.js
@@ -1,9 +1,28 @@
 angular.module("portal_app")
     .controller("DashboardController", ["$scope","$http", function ($scope, $http) {
         $scope.myCars = [];
+        $scope.alerts = [];
+        $scope.verif = [];
 
-        $http.get('/cars').success(function (response) {
+        $http.get('/carFleet/myFlee').success(function (response) {
             $scope.myCars = response;
+
+            $scope.alerts = response.carFleet.filter(function(e) {
+                return e.map(function(m) {
+                    return m.status.toLowerCase();
+                }).filter(function (f) {
+                    return f == "alert"
+                }).length > 0
+            });
+
+            $scope.alerts = response.carFleet.filter(function(e) {
+                return e.map(function(m) {
+                        return m.status.toLowerCase();
+                    }).filter(function (f) {
+                        return f == "verificare"
+                    }).length > 0
+            });
+
         });
 
     }]);
