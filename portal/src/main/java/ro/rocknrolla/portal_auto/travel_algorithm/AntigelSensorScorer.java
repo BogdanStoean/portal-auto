d68232ae999b347b814485a47a83097e272b8724
borceavirgil@gmail.com <virgil21>
2015-06-07 05:38:29 +0300

    * cleanup

--- portal/src/main/java/ro/rocknrolla/portal_auto/travel_algorithm/AntigelSensorScorer.java
+++ portal/src/main/java/ro/rocknrolla/portal_auto/travel_algorithm/AntigelSensorScorer.java
@@ -1,14 +1,22 @@
 package ro.rocknrolla.portal_auto.travel_algorithm;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ro.rocknrolla.portal_auto.repositories.CarHistoryRepository;
 
 @Component
 public class AntigelSensorScorer implements SensorScoring {
 
+    @Autowired
+    private CarHistoryRepository carHistoryRepository;
+
     @Override
-    public int scoreIt(Long carId, String sensor, SensorPriority sensorPriority) {
+    public int scoreIt(Long carId, String sensor, SensorPriority sensorPriority, Long KM) {
         double sensorPriorityPoints = sensorPriority.getPoints();
+
+//        carHistoryRepository.findLastSensorValuesForCar()
+
         return 0;
     }
 }
--- portal/src/main/java/ro/rocknrolla/portal_auto/travel_algorithm/SensorScoring.java
+++ portal/src/main/java/ro/rocknrolla/portal_auto/travel_algorithm/SensorScoring.java
@@ -3,5 +3,5 @@ package ro.rocknrolla.portal_auto.travel_algorithm;
 
 public interface SensorScoring {
 
-    int scoreIt(Long carId, String sensor, SensorPriority sensorPriority);
+    int scoreIt(Long carId, String sensor, SensorPriority sensorPriority, Long KM);
 }
